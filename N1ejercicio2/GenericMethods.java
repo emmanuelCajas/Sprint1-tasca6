9c47733fc5346d740125411d95bb059764a120d1
Emmanuel Cajas <emmanuelcajasballesteros@gmail.com>
2023-03-05 06:39:42 +0100

    Agregando tasca6

---
+++ N1ejercicio2/GenericMethods.java
@@ -0,0 +1,10 @@
+package tasca6.N1ejercicio2;
+
+public class GenericMethods {
+
+	public static <T,K,V> void metodoGenerico(T objeto1, K objeto2, V objeto3) {
+		System.out.println(objeto1 + "//"+ objeto2 + "//" + objeto3);
+	};
+	
+	
+}
